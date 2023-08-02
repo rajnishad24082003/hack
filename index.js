b1b4054ecfffeeff2b5e495ffd46ca75c5d64911
raj nishad <rajnishad24082003@gmail.com>
2023-08-03 00:09:13 +0530

    screen shot done

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-const path = require("path");
-const fs = require("fs");
 const express = require("express");
+const http = require("http");
+const fs = require("fs");
+const socketIO = require("socket.io");
+const path = require("path");
+const ss = require("desktop-screenshot");
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const PORT = 3000;
-app.use(express.static(path.join(__dirname, "assets")));
-const camera = require("./function/camera");
-const screenshot = require("./function/screenshot");
 
-app.get("/", (req, res) => {
-  res.send("home page");
-});
-app.get("/camera", (req, res) => {
-  camera(req, res);
-});
-app.get("/screenshot", (req, res) => {
-  res.setHeader("Content-Type", "image/png");
-  res.setHeader("Cache-Control", "no-cache");
-  res.setHeader("Connection", "keep-alive");
-  screenshot(req, res);
-  req.on("close", () => {
-    console.log("Client disconnected");
+io.on("connection", (socket) => {
+  console.log("A client connected");
+  //sockets-start
+  socket.on("RepeatScreenShotTake", (data) => {
+    ss("ss/screenshot.png", (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const imageData = fs.readFileSync(
+          path.join(__dirname, "ss", "screenshot.png")
+        );
+        io.emit("RepeatScreenShotGet", imageData);
+      }
+    });
+  });
+  //sockets-end
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
   });
 });
+app.get("/", (req, res) => {
+  res.status(200).end("home page");
+});
+app.get("/screenshot", (req, res) => {
+  ss("ss/screenshot.png", (error) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+  });
+  res.sendFile(path.join(__dirname, "public", "screenshot.html"));
+});
 
-app.listen(PORT, () => {
-  console.log(`server listening on port : ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
